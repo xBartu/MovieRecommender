112212bbb8ac3f891233b32107944f5071750ba6
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-05-19 14:31:13 +0300

    movie scaffold

---
+++ movie-rec/db/migrate/20180519113056_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :desc
+      t.string :photo_url
+      t.references :genre, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
