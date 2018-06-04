842e5dcf4c8a0a3d0a9c44a2f08c9c55091404dd
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-04 23:55:39 +0300

    todo added to index method on the genres' controller

--- movie-rec/app/controllers/genres_controller.rb
+++ movie-rec/app/controllers/genres_controller.rb
@@ -1,7 +1,10 @@
 class GenresController < ApplicationController
     def index
+        #list all the genres
+        # TODO introduce theme
         @genres = Genre.all
     end
+
     def show
     end
 end
