4f717c8da9252e799b911df04e0286ec8eef2163
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-07 20:56:11 +0300

    created edit and update method, and possible typo fixed at create method

--- movie-rec/app/controllers/roles_controller.rb
+++ movie-rec/app/controllers/roles_controller.rb
@@ -2,11 +2,22 @@ class RolesController < ApplicationController
 	def new
 		@role = Role.new
 	end
-	
+
 	def create
 		@role = Role.new(role_params)
-		if not(@role.save)
+		if(not @role.save)
 			render 'new'
+		end
+	end
+
+	def edit
+		@role = Role.find(params[:id])
+	end
+
+	def update
+		@role = Role.find(params[:id])
+		if(not @role.update(role_params))
+			render 'edit'
 		end
 	end
 
