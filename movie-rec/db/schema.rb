974663e043325e8435c95b2e995048846e411023
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-07 20:24:39 +0300

    created role model

--- movie-rec/db/schema.rb
+++ movie-rec/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_203808) do
+ActiveRecord::Schema.define(version: 2018_06_07_172055) do
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
