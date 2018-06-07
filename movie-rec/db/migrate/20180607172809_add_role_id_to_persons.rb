308575fc64aee57597d4eab333e90c2575cde49b
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-07 20:29:04 +0300

    one to many between person and role

---
+++ movie-rec/db/migrate/20180607172809_add_role_id_to_persons.rb
@@ -0,0 +1,4 @@
+class AddRoleIdToPersons < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- movie-rec/db/schema.rb
+++ movie-rec/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_172524) do
+ActiveRecord::Schema.define(version: 2018_06_07_172809) do
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
