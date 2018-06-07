d94b4ad9cd8fd4883c9c6cad05517ed51f07c61b
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-07 20:58:11 +0300

    comments on roles controller

--- movie-rec/app/controllers/roles_controller.rb
+++ movie-rec/app/controllers/roles_controller.rb
@@ -1,27 +1,34 @@
 class RolesController < ApplicationController
 	def new
+		# new method for new page
 		@role = Role.new
 	end
 
 	def create
+		# create method
 		@role = Role.new(role_params)
+		# check the rule!
 		if(not @role.save)
 			render 'new'
 		end
 	end
 
 	def edit
+		# edit method
 		@role = Role.find(params[:id])
 	end
 
 	def update
+		# update method
 		@role = Role.find(params[:id])
+		# check rules
 		if(not @role.update(role_params))
 			render 'edit'
 		end
 	end
 
 	private def role_params
+		# no need for hidden inputs!
 		params.require(:role).permit(:title)
 	end
 end
