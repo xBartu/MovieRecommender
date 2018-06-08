bc76934c5c428e8db1af88488da8209c6b7d130b
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-08 13:26:39 +0300

    create new, create and person_params methods

--- movie-rec/app/controllers/people_controller.rb
+++ movie-rec/app/controllers/people_controller.rb
@@ -1,2 +1,20 @@
 class PeopleController < ApplicationController
+	def new
+		# new method
+		@person = Person.new
+	end
+
+	def create
+		# create method
+    	# Warning: no user interaction
+		@person = Person.new(person_params)
+		# check the rule!
+		if(@person.save)
+			redirect_to @person
+		else
+			render 'new'
+	end
+
+	private def person_params
+		params.require(:person).permit(:photo_url, :first_name, :last_name, :bio, :role_id)
 end
