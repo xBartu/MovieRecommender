92f96bddd2a2c0b4896b182771e56755256b2c03
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-08 18:07:28 +0300

    added has_and_belongs_to_many to movies, people

--- movie-rec/app/models/movie.rb
+++ movie-rec/app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   belongs_to :genre
+  has_and_belongs_to_many :people
 end
--- movie-rec/app/models/person.rb
+++ movie-rec/app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
   belongs_to :role
   has_and_belongs_to_many :users
+  has_and_belongs_to_many :movies
 end
