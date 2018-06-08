e55a5305ae385edfe9530acfbe393389c2ae014c
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-08 14:28:52 +0300

    created many2many

--- movie-rec/app/models/person.rb
+++ movie-rec/app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ApplicationRecord
   belongs_to :role
+  has_and_belongs_to_many :users
 end
--- movie-rec/app/models/user.rb
+++ movie-rec/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :genres
+  has_and_belongs_to_many :people
 end
