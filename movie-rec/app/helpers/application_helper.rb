869630f49af7703b5925c380cb63516775aa94a5
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-06-09 20:43:04 +0300

    added the number of notification

--- movie-rec/app/helpers/application_helper.rb
+++ movie-rec/app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def notification_count
+		# TODO START {convert it to sql}
+		arr = Array.new
+		current_user.people.each do |person|
+			person.movies.each do |movie|
+				if movie.created_at > current_user.last_notification
+					arr.push(movie) 
+				end
+			end
+		end
+		movies = Movie.where(genre_id:current_user.genres).where("created_at>?",current_user.last_notification)
+		movies.each do |movie|
+			arr.push(movie)
+		end
+		arr.count
+		# TODO END
+	end
 end
